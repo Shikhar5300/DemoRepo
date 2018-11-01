2d61e9f6ec3d8f47026a3dc485cb9d9835f15bc7
shikhar <shikhar5300@gmail.com>
2018-11-02 05:15:12 +0530

    some init msg

---
+++ src/test/java/com/qa/tests/DemoTest.java
@@ -0,0 +1,37 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void Sum() {
+		System.out.println("Sum");
+		int a= 10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+	}
+	@Test
+	public void Sub() {
+		System.out.println("Sub");
+		int a= 10;
+		int b=20;
+		Assert.assertEquals(10, b-a);
+	}
+	@Test
+	public void Multi() {
+		System.out.println("Multi");
+		int a= 10;
+		int b=20;
+		Assert.assertEquals(200, a*b);
+	}
+	@Test
+	public void Div() {
+		System.out.println("Div");
+		int a= 10;
+		int b=20;
+		Assert.assertEquals(2, b/a);
+	}
+
+}
